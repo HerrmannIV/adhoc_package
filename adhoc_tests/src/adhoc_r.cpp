0cc5efcf22ddb1407cfcd3ba99b07af244af574a
Marcel Mann <marcelmannmm@freenet.de>
2017-03-14 15:51:33 +0100

    receiver kann auch string

--- adhoc_tests/src/adhoc_r.cpp
+++ adhoc_tests/src/adhoc_r.cpp
@@ -1,14 +1,21 @@
 #include "ros/ros.h"
 #include "adhoc_customize/include.h"
+#include "std_msgs/String.h"
 
 void rectangleCallback(const adhoc_customize::Rectangle::ConstPtr& msg){
-	ROS_INFO("I heard: [%c][%c]", msg->length, msg->width);
+	ROS_INFO("I heard: [%d][%d]", msg->length, msg->width);
+}
+
+void stringCallback(const 
+	std_msgs::String::ConstPtr& msg){
+	ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "adhoc_receiver_node");  
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("t_rectangle", 1000, rectangleCallback);  
+	ros::Subscriber sub_r = n.subscribe("t_rectangle", 1000, rectangleCallback);  
+	ros::Subscriber sub_s = n.subscribe("t_string", 1000, stringCallback);  
 	ros::spin();  
 	return 0;
 }
