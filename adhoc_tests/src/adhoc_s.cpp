b99979cf86bd09e91cec8afb9e355957ff95d19c
Marcel Mann <marcelmannmm@freenet.de>
2017-03-27 18:08:07 +0200

    im sender ping und string getrennt

--- adhoc_tests/src/adhoc_s.cpp
+++ adhoc_tests/src/adhoc_s.cpp
@@ -52,15 +52,16 @@ int main (int argc, char **argv){
 	ros::NodeHandle nh;
 
 	// get Parameters and print INFO
-	int rate, loop, mode_i,strLen;
-	std::string dst_car, filename;	
+	int rate, loop, mode_i,strLen, sleep;
+	std::string dst_car, filename;
+	nh.getParam("/sender/sleep", sleep);
 	nh.getParam("/sender/dst_car", dst_car);
 	nh.getParam("/sender/rate", rate);
 	nh.getParam("/sender/mode", mode_i);
 	nh.getParam("/sender/loop", loop);
 	nh.getParam("/sender/strLen", strLen);
 	nh.getParam("/sender/filename", filename);
-	ROS_INFO("loop [%d]; mode [%d]: rate [%d]; length/10 [%d], Dest: [%s]", loop, mode_i, rate, strLen, dst_car.c_str());
+	ROS_INFO("loop [%d]; mode [%d]: rate [%d]; length/10 [%d], Dest: [%s], sleep:[%d]", loop, mode_i, rate, strLen, dst_car.c_str(), sleep);
 	Mode mode = static_cast<Mode>(mode_i);
 	ros::Rate loop_rate(rate);
 	adhoc_customize::Rectangle rectangle;
@@ -87,37 +88,24 @@ int main (int argc, char **argv){
 
 	while(ros::ok() && i<loop){
 		i++;
-		if(mode==STRING_SERIALIZE){
-			// send String with my own Serialization method
-			std_msgs::String str;
-			str.data = longstring;
-			adhoc_communication::sendMessage(str, FRAME_DATA_TYPE_STRING, dst_car, "t_String");
-		}else if(mode==STRING_SERVICE || ping){
-			// send String with native sendString Service or empty string as Ping
+		if(mode==PING){
+			// setup ROS-stuff
 			ros::ServiceClient client = nh.serviceClient<adhoc_communication::SendString>("adhoc_communication/send_string");
 	    	adhoc_communication::SendString srv;
-	    	std::stringstream ss;
-	    	ss << i;
+	    	// setup message, data is topic of returned ping
+	    	std::stringstream ss; ss << i;
 	    	std::string returnTopic = "top_ping" + ss.str();
-	    	// fill Service-Fields
-		    srv.request.topic = ping ? "t_ping" : "t_string";
-		    srv.request.data = ping ? returnTopic : longstring;
+		    srv.request.topic = "t_ping";
+		    srv.request.data = returnTopic;
 		    srv.request.dst_robot = dst_car;
-		    		    
-		    // call Service
-		    if(ping) {
-		    	beforePing = ros::Time::now();
-		    	//ROS_INFO("BeforeX: [%f] sec", beforePing.toSec());
-		    }
-
-		    if (client.call(srv)){
-		        //ROS_INFO("Response.status: %d", srv.response.status);
-		    }else{
-		        ROS_ERROR("Failed to call PING/STRING-service");
-		    }
-		    if (ping){
+		    
+		    //PING
+		    beforePing = ros::Time::now();
+		    if (!client.call(srv)) ROS_ERROR("Failed to call PING/STRING-service");
 			ros::topic::waitForMessage<adhoc_communication::RecvString>(returnTopic , ros::Duration(2));
 			ros::Time afterPing = ros::Time::now();
+
+			// calc Latency and output (to file)
 			ros::Duration pingTime = afterPing - beforePing;
 			float pingTimeSec = pingTime.toSec();
 			if (pingTimeSec > 1.5)
@@ -125,13 +113,29 @@ int main (int argc, char **argv){
 			else
 				myfile << pingTimeSec <<"\n";
 
-		}
+		}else if(mode==STRING_SERIALIZE){
+			// send String with my own Serialization method
+			std_msgs::String str;
+			str.data = longstring;
+			adhoc_communication::sendMessage(str, FRAME_DATA_TYPE_STRING, dst_car, "t_String");
+		}else if(mode==STRING_SERVICE){
+			// send String with native sendString Service 
+			ros::ServiceClient client = nh.serviceClient<adhoc_communication::SendString>("adhoc_communication/send_string");
+	    	adhoc_communication::SendString srv;
+	    	// fill Service-Fields
+		    srv.request.topic = "t_string";
+		    srv.request.data = longstring;
+		    srv.request.dst_robot = dst_car;
+		    		    
+		    // call Service
+		    if (!client.call(srv)) ROS_ERROR("Failed to call PING/STRING-service");
 
 		}else if(mode==RECT){
 			// send Rectangle
 			rectangle.length = i;
 			rectangle.width = i;
 			adhoc_communication::sendMessage(rectangle, FRAME_DATA_TYPE_RECTANGLE, dst_car, "t_rectangle");
+
 		}else if(mode==STUD){
 			adhoc_customize::Student stud;
 			stud.name = "Rau";
@@ -140,7 +144,7 @@ int main (int argc, char **argv){
 			stud.matnr = 123;
 			adhoc_communication::sendMessage(stud, FRAME_DATA_TYPE_STUDENT, dst_car, "t_stud");
 		}
-		//if (ping) loop_rate.sleep();	    
+		if (sleep) loop_rate.sleep();
 	}	
 
 	ros::Time end = ros::Time::now();
