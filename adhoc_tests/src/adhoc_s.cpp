829d0fa1cf37a5866c5a4ecc2a8ecd5cb9189e8b
Marcel Mann <marcelmannmm@freenet.de>
2017-03-30 12:21:18 +0200

    ehm ja, umbenannt in sender mirror receiver

--- adhoc_tests/src/adhoc_s.cpp
+++ adhoc_tests/src/adhoc_s.cpp
@@ -21,11 +21,10 @@ enum Mode{
 };
 
 ros::Time beforePing;
-bool ping, received;
+bool ping;
 std::ofstream outFile;
 adhoc_customize::Rectangle rectangle;
 std::string longstring;
-double pingTimeD;
 int recvCounter = 0, linecounter = 0;
 
 
@@ -45,47 +44,16 @@ void convertToPrefixString(int input, std::string &output){
 	len_s.erase(len_s.end()-crop, len_s.end());
 	output = len_s + prefix;
 }
-/*
-void pingCallback(const adhoc_customize::RecvTime::ConstPtr& recvTime){
-	if(recvCounter){
-		ros::Time afterPing = ros::Time::now();
-		float pingTimeSec = (afterPing - recvTime->time).toSec();
-		ROS_INFO("Ping duration: [%f] sec", pingTimeSec);
-		outFile << pingTimeSec <<";";
-		linecounter++;
-	}
-	if(linecounter == 25){
-		linecounter=0;
-		outFile << "\n";
-	}
-	recvCounter++;
-}
-*/
 
-void answerCallback(const adhoc_customize::RecvTime::ConstPtr& recvTime){
-	if(recvCounter){
-		ros::Time afterSend = ros::Time::now();
-		float sendTimeSec = (afterSend - recvTime->time).toSec();
-		ROS_INFO("RecvAnswer, Duration: [%f] sec", sendTimeSec);
-		outFile << sendTimeSec <<";";
-		linecounter++;
-	}
-	if(linecounter == 25){
-		linecounter=0;
-		outFile << "\n";
-	}
-	recvCounter++;
-}
 
 int main (int argc, char **argv){
 	
 	ros::init(argc, argv, "adhoc_sender1");
-	ros::NodeHandle nh;
-	ros::Subscriber sub_answer = nh.subscribe("t_answer", 1000, answerCallback);  
-	//ros::Subscriber sub_string = nh.subscribe("t_str", 1000, sendCallback);  
-	ros::AsyncSpinner  spinner(1);
-	spinner.start();
-	
+	ros::NodeHandle nh; 
+	ros::Publisher confPub = nh.advertise<std_msgs::String>("t_filename", 1000);
+	ros::AsyncSpinner spinner(1);
+	//spinner.start();
+
 	// get Parameters and print INFO
 	int rate, loop, mode_i, strLen, sleep;
 	std::string dst_car;
@@ -122,33 +90,14 @@ int main (int argc, char **argv){
 		confStringStream 	<< "r" << rate;
 	if (mode == STRING_SERIALIZE || mode == STRING_SERVICE) 
 		confStringStream << "le" << strLen;
-	
-	/*
-	std::ifstream inFile(fname.c_str());
-	std::string inputLine;
-	if (inFile.is_open()){
-		getline(inFile, inputLine);
-		inFile.close();
-		//std::cout << "|" << inputLine << "|\n";
-		//std::cout << "|" << confStringStream.str() << "|\n";
-		if (confStringStream.str().compare(inputLine) == 0)
-			ROS_INFO("OutputFile same Config");
-		else{ 
-			return 0;
-		}
-	}else{
-		ROS_INFO("Not open");
-		outFile.open (fname.c_str());
-		outFile << confStringStream.str() << "\n";
-		outFile.close();		
-	}
-	*/
-	ROS_INFO("%s",confStringStream.str().c_str());
-	std::string fname = std::string("/home/pses/catkin_ws/src/adhoc_package/"+ confStringStream.str() +".csv");
-	outFile.open (fname.c_str(), std::ios_base::app);
+
+	std_msgs::String filepath;
+	filepath.data = std::string("/home/pses/catkin_ws/src/adhoc_package/"+ confStringStream.str() +".csv");
+	ROS_INFO("[%s]", filepath.data.c_str());
+	confPub.publish(filepath);
+	spinner.start();
 	int i = 0;
 
-	//ros::Time begin = ros::Time::now();
 	while(ros::ok() && i<loop){
 		i++;
 		if(mode==PING_ALT){
@@ -160,7 +109,7 @@ int main (int argc, char **argv){
 			adhoc_customize::StringWTime strWTime;
 			strWTime.data = longstring;
 			strWTime.time = ros::Time::now();
-			adhoc_communication::sendMessage(strWTime, FRAME_DATA_TYPE_STRING_W_TIME, dst_car, "t_stringSerializedWTime");
+			adhoc_communication::sendMessage(strWTime, FRAME_DATA_TYPE_STRING_W_TIME, dst_car, "t_sswt");
 		}else if(mode==STRING_SERVICE){
 			// send String with native sendString Service 
 			ros::ServiceClient client = nh.serviceClient<adhoc_communication::SendString>("adhoc_communication/send_string");
@@ -189,16 +138,5 @@ int main (int argc, char **argv){
 		}
 		if (sleep) loop_rate.sleep();
 	}	
-	/*
-	ros::Time end = ros::Time::now();
-	ros::Duration dur = end-begin;
-	ROS_INFO("duration: %f sec", dur.toSec());
-	*/
-	while(recvCounter<=loop){
-		ROS_INFO("COUNTER: [%d]", recvCounter);
-		loop_rate.sleep();
-	}
-	outFile << "\n";
-	outFile.close();
 	return 1;
 }
--- adhoc_tests/src/timeRecv.cpp
+++ adhoc_tests/src/timeRecv.cpp
@@ -1,7 +1,5 @@
 #include "ros/ros.h"
 #include "adhoc_customize/include.h"
-#include <iostream> 
-#include <fstream>
 
 std::ofstream outFile;
 int recvCounter = 0, linecounter = 0;
@@ -20,13 +18,12 @@ void answerCallback(const adhoc_customize::RecvTime::ConstPtr& recvTime){
 		outFile << "\n";
 	}
 	recvCounter++;
-}    
+}
 void filenameCallback(const std_msgs::String::ConstPtr& msg){
-	outFile << "\n";
-	outFile.close();
-	recvCounter = 0; linecounter = 0;
-	outFile.open (msg->data.c_str(), std::ios_base::app);
-	ROS_INFO("Filepath: [%s]", msg->data.c_str());
+	outfile.close();
+	std::string fname = std::string("/home/pses/catkin_ws/src/adhoc_package/"+ msg->data.c_str() +".csv");
+	outFile.open (fname.c_str(), std::ios_base::app);
+	ROS_INFO("File changed to [%s]", fname.c_str());
 }
 
 
@@ -34,13 +31,8 @@ int main (int argc, char **argv){
 	ros::init(argc, argv, "adhoc_timeRecv");
 	ros::NodeHandle nh;
 	ros::Subscriber sub_answer = nh.subscribe("t_answer", 1000, answerCallback); 
-	ros::Subscriber sub_filename = nh.subscribe("t_filename", 1000, filenameCallback); 
+	ros::Subscriber sub_answer = nh.subscribe("t_filename", 1000, filenameCallback); 
 	ros::spin();
-	/*
-	ros::AsyncSpinner spinner(2);
-	spinner.start();
-	ros::waitForShutdown();
-	*/
 	outFile.close();
 	return 1;
 }
