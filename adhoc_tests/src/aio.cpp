7e0d0755f56aeb6a4b85b174b9f14c6ca6e61a5a
Marcel Mann <marcelmannmm@freenet.de>
2017-04-13 19:14:24 +0200

    timeout wieder dazu

--- adhoc_tests/src/aio.cpp
+++ adhoc_tests/src/aio.cpp
@@ -20,13 +20,10 @@ enum State{
 };
 
 enum Mode{
-	PING,
-	STRING_SERIALIZE,
-	STRING_SERVICE,
-	RECT,
-	STUD,
-	PING_ALT
+	PING = 5,
+	DATA = 1
 };
+std::string size;
 int timeoutCounter;
 std::ofstream outFile;
 int receivedFirst = 0, linecounter = 0, readingCounter=0;
@@ -40,6 +37,8 @@ int loop, mode_i, strLen;
 std::string dst_car, pos, longstring;
 
 std_msgs::Time timeMsg;
+adhoc_customize::StringWTime strWTime;
+double timeoutTime;
 double valArray[1000];
 double sum = 0.0, avg, thresh;
 double quart1, med, quart3, quart90, min, max, overx = 0.0, timeoutRate;
@@ -98,7 +97,7 @@ int main (int argc, char **argv){
 	ros::Subscriber sub_answer = nh.subscribe("t_answer", 1000, answerCallback); 
 	ros::AsyncSpinner spinner(4);
 	spinner.start();
-	ros::Rate rate(100);
+
 	// get Parameters and print INFO
 	nh.getParam("/sender/dst_car", dst_car);
 	nh.getParam("/sender/mode", mode_i);
@@ -106,28 +105,28 @@ int main (int argc, char **argv){
 	nh.getParam("/sender/strLen", strLen);
 	nh.getParam("/sender/pos", pos);
 	nh.getParam("/sender/thresh", thresh);
+	nh.getParam("/sender/timeout", timeoutTime);
 	ROS_INFO("loop [%d], mode [%d], length/10 [%d], \n Dest: [%s], pos: [%s]", loop, mode_i, strLen, dst_car.c_str(), pos.c_str());
 	
 	Mode mode = static_cast<Mode>(mode_i);
 	ping = (mode == PING);
 
+
 	// dummy is 10Bytes, make longstring=strLen*10
-	if (mode == STRING_SERIALIZE || mode == STRING_SERVICE){
+	if (mode == DATA){
 		std::string dummy = "ABCDEFGHIJ";
 		for(int k = 0; k<strLen; k++)
 			longstring += dummy;
-		std::string size;
 		convertToPrefixString(longstring.length(), size);	
 		std::cout << "Stringlength: "<< size << "Bytes\n";
+		strWTime.data = longstring;
 	}
 
 	// Generate Config-String
 	std::ostringstream confStringStream;
-	//if (pos != "")
-		//confStringStream	<< "p" << pos;
 	confStringStream	<< "m" << mode_i;
-	if (mode == STRING_SERIALIZE || mode == STRING_SERVICE) 
-		confStringStream << "le" << strLen;
+	//if (mode == DATA)
+	//	confStringStream << "le" << strLen;
 
 	// make absolute filepath and open file
 	std::string filepath = std::string("/home/pses/catkin_ws/src/adhoc_package/"+ confStringStream.str() +".csv");
@@ -142,31 +141,42 @@ int main (int argc, char **argv){
 		switch (state){
 		case SEND:
 			sentTime = ros::Time::now();
-			timeMsg.data = sentTime;
-			adhoc_communication::sendMessage(timeMsg, FRAME_DATA_TYPE_TIME, dst_car, "t_ping");
+			switch (mode){
+				case PING:					
+					timeMsg.data = sentTime;
+					adhoc_communication::sendMessage(timeMsg, FRAME_DATA_TYPE_TIME, dst_car, "t_ping");
+					break;
+				case DATA:					
+					strWTime.time = sentTime;
+					adhoc_communication::sendMessage(strWTime, FRAME_DATA_TYPE_STRING_W_TIME, dst_car, "t_sswt");
+					break;
+			}
 			nextState = WAIT_FOR_ANSWER;
+
 			break;
+
 		case WAIT_FOR_ANSWER:
 			
 			span = ros::Time::now() - sentTime;
 			
-			//timeout = (span.toSec() > 1.0);
-			//if (timeout || received){
+			timeout = (span.toSec() > timeoutTime);
+			if (mode == DATA) timeout = false;
 
-			if ((span.toSec() > 1.0) || received){
+			if (timeout || received){
 
-				/*
+			//if ((span.toSec() > 1.0) || received){
+
+				
 				if (timeout) {
 					timeoutCounter++;
 					ROS_ERROR("TIMEOUT");
-				}*/
+				}
 				received = false;
 				timeout = false;
 				nextState = SEND;			
 			}
 			else{
 				nextState = state;
-				//rate.sleep();
 			}
 			break;
 		case FINISH:
@@ -201,6 +211,7 @@ int main (int argc, char **argv){
 			outFile.open (filepath.c_str(), std::ios_base::app);
 			outFile << pos << ";";
 			outFile << loop << ";";
+			if (mode = DATA) outFile << size << ";";
 			outFile << avg << ";";
 			outFile << min << ";";
 			outFile << quart1 << ";";
